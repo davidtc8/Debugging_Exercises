a1e5fd362f7514c4a4a67862a569326a2d9a8bc2
David Torres <davidtorresc8@gmail.com>
2020-12-07 11:32:51 -0600

    Exercise 3

--- Exercise 3/exercise3.py
+++ Exercise 3/exercise3.py
@@ -5,6 +5,8 @@
 # Play Computer
 year = int(input("What's your year of birth?"))
 if year > 1980 and year < 1994:
-    print("You are a millenial.")
+  print("You are a millenial.")
 elif year > 1994:
-    print("You are a Gen Z.")
+  print("You are a Gen Z.")
+
+#Issue: The 'if' statement won't run
