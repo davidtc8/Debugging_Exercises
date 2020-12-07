ef54bc1ea5110e824c5a97dd1b79eb45cdefacb7
David Torres <davidtorresc8@gmail.com>
2020-12-07 11:30:23 -0600

    Exercise 3

---
+++ Exercise 3/exercise3.py
@@ -0,0 +1,10 @@
+############DEBUGGING#####################
+
+# Describe Problem
+
+# Play Computer
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+    print("You are a millenial.")
+elif year > 1994:
+    print("You are a Gen Z.")
