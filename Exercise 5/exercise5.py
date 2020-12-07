2fe23473b50b90e15db2f3b175730453a060a641
David Torres <davidtorresc8@gmail.com>
2020-12-07 12:06:25 -0600

    Exercise 5

---
+++ Exercise 5/exercise5.py
@@ -0,0 +1,12 @@
+############DEBUGGING#####################
+
+#Describe Problem
+#Print is Your Friend
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page == int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
+
+#Issue: The total_words won't print out! ): 
