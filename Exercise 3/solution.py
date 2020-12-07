8200fb8640067aa1174d47f33a990fafab94a0af
David Torres <davidtorresc8@gmail.com>
2020-12-07 11:37:45 -0600

    Solution to Exercise 3

--- Exercise 3/solution.py
+++ Exercise 3/solution.py
@@ -6,7 +6,8 @@
 year = int(input("What's your year of birth?: "))
 if year > 1980 and year < 1994:
     print("You are a millenial.")
-elif year > 1994:
+elif year >= 1994:
     print("You are a Gen Z.")
 
 #Solution: The indentation was wrong! In Python there should be indentation
+#Also, when you run 1994 it didn't run, that's because of the parameters, we have to put '>=' sign
