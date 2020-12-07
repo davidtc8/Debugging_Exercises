b45f4755583831988484761c8ef2e50636a61bc0
David Torres <davidtorresc8@gmail.com>
2020-12-07 11:41:02 -0600

    Exercise 4

---
+++ Exercise 4/exercise4.py
@@ -0,0 +1,9 @@
+############DEBUGGING#####################
+
+# Describe Problem
+# Fix the Errors
+age = input("How old are you?")
+if age > 18:
+print("You can drive at age {age}.")
+
+#Issue: The program won't run!
