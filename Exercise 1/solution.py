8e6adf84e123169822c776e14fbd04c4f4705e50
David Torres <davidtorresc8@gmail.com>
2020-12-07 10:39:00 -0600

    First Exercise

---
+++ Exercise 1/solution.py
@@ -0,0 +1,9 @@
+############DEBUGGING#####################
+
+# # Describe Problem
+def my_function():
+    for i in range(1, 20):
+        if i == 20:
+            print("You got it")
+
+my_function()
